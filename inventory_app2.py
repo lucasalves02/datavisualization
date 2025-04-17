bb8a8ec0ce54e8146c15da1481f93867dc15f746
lucasalves02 <63018939+lucasalves02@users.noreply.github.com>
2025-04-17 16:08:45 -0300

    Update inventory_app2.py

--- inventory_app2.py
+++ inventory_app2.py
@@ -146,7 +146,7 @@ def plot_absolute_performance(input_df):
 
     df = input_df.copy()
     # Uses Fixed Figure Size
-    fig, axes = plt.subplots(1, 2, figsize=(16, 6))
+    fig, axes = plt.subplots(2, 1, figsize=(6, 16))
 
     try:
         # Essential columns check
